0bec4a8d8447df386ec8085458633eddebd84ce6
Juan Esteban Nichoy L <juan.nichoy@mawesi.net>
2020-12-27 22:30:30 -0500

    Modificación de tabla variable nodo sesnor agregando la columna detalles y creaciacón de las querys para varibles nodo sensor

---
+++ Querys/Query Variables_nodo_sensor.sql
@@ -0,0 +1,37 @@
+/*
+    Autor:          Juan Esteban Nichoy Larrañaga
+    Fecha:          25/12/2020
+    Ejecutar en:    BD_Proyecto_Grado
+    Descripción:    Querys para las variables del nodo sensor.
+*/
+
+--Crear una nueva variable
+INSERT 
+    INTO VARIABLES_NODO_SENSOR
+    (NOMBRE_VARIABLE, DETALLES, TIPO_DATO, UNIDAD_MEDIDA, RANGO_MIN, RANGO_MAX, ESTADO, FECHA_CREACION, HORA_CREACION)
+VALUES
+    ('TEMPERATURA AMBIENTAL', 'Sensa la temperatura ambiental donde se encuentra el nodo sensor','DOUBLE', '°C', '13.0', '40.0', true, CURDATE(), CURTIME());
+
+--Consultar variables Nodo Sensor 
+SELECT * FROM VARIABLES_NODO_SENSOR;
+
+--Consultar variable nodo sensor por nombre variable
+SELECT * FROM VARIABLES_NODO_SENSOR
+    WHERE NOMBRE_VARIABLE = ? 
+
+--Consultar variable nodo sensor por id variable
+SELECT * FROM VARIABLES_NODO_SENSOR
+    WHERE ID_VARIABLE = ? 
+
+--Actualizar una varibale por ID
+UPDATE VARIABLES_NODO_SENSOR
+    SET NOMBRE_VARIABLE = ?,
+        DETALLES = ?,
+        TIPO_DATO = ?,
+        UNIDAD_MEDIDA = ?,
+        RANGO_MIN = ?,
+        RANGO_MAX = ?,
+        ESTADO = ?,
+        FECHA_ACTUALIZACION = CURDATE(),
+        HORA_ACTUALIZACION = CURTIME()
+    WHERE ID_VARIABLE = ?
