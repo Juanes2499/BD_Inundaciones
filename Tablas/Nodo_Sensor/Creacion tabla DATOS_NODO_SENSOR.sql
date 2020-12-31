686266352f3b28eb686f79ebe50224569a8e00cf
Juan Esteban Nichoy L <juan.nichoy@mawesi.net>
2020-12-30 22:12:20 -0500

    Se modifica la tabla Datos_Nodo_Sensor y se crea la query para Datos_Nodo_Sensor

--- Tablas/Nodo_Sensor/Creacion tabla DATOS_NODO_SENSOR.sql
+++ Tablas/Nodo_Sensor/Creacion tabla DATOS_NODO_SENSOR.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS `DATOS_NODO_SENSOR` (
 	`ID_DATO` int(11) NOT NULL AUTO_INCREMENT,
 	`ID_NODO_SENSOR` int not null, 
 	`ID_VARIABLE` int not null,
-	`VALOR_MEDIDO` varchar(10) not null,
+	`VALOR_DATO` varchar(10) not null,
 	`FECHA_CREACION` date NOT NULL,
 	`HORA_CREACION` time NOT NULL,
 	primary key (ID_DATO),
