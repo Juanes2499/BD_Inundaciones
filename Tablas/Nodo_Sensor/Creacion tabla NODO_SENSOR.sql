eabee3fd24aec729e18042a0aa78d65afd4016eb
Juan Esteban Nichoy L <juan.nichoy@mawesi.net>
2021-01-19 21:19:38 -0500

    Modificacion en las tablas en el id de cada módulo

--- Tablas/Nodo_Sensor/Creacion tabla NODO_SENSOR.sql
+++ Tablas/Nodo_Sensor/Creacion tabla NODO_SENSOR.sql
@@ -6,15 +6,15 @@
 */
 
 CREATE TABLE IF NOT EXISTS `NODO_SENSOR` (
-	`ID_REGISTRO` int(10) NOT NULL AUTO_INCREMENT,
   	`ID_NODO_SENSOR` varchar(100) NOT NULL,
 	`TOKEN` longtext NOT NULL,
 	`LATITUD` double NOT NULL,
 	`LONGITUD` double NOT NULL,
+	`DISPOSITIVO_ADQUISICION` varchar(500) not null,
 	`ESTADO` bool NOT NULL,
 	`FECHA_CREACION` date NOT NULL,
 	`HORA_CREACION` time NOT NULL,
 	`FECHA_ACTUALIZACION` date default null,
 	`HORA_ACTUALIZACION` time default null, 
-	primary key (ID_REGISTRO, ID_NODO_SENSOR)
+	primary key (ID_NODO_SENSOR)
 );
