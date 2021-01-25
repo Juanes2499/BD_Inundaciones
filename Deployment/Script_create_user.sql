f62e7664fc05d5444257a23d3db548ba96b08e6f
Juan Esteban Nichoy L <juan.nichoy@mawesi.net>
2021-01-25 15:17:54 -0500

    Actualización para deployment de dockerfile para dockercompose

---
+++ Deployment/Script_create_user.sql
@@ -0,0 +1,8 @@
+/*
+    Autor:          Juan Esteban Nichoy Larrañaga
+    Fecha:          25/01/2021
+    Ejecutar en:    Script para Dockerfile
+    Descripción:    Crear usuario para la API
+*/
+
+CREATE USER 'DBATESTS' IDENTIFIED BY 'DBATESTS2021';
