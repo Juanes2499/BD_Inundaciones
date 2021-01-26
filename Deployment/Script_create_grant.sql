f30a6d551d69e7bf4d1d7e8ba23e2476550ec765
Juan Esteban Nichoy L <juan.nichoy@mawesi.net>
2021-01-26 09:14:22 -0500

    Actulización para deployment dev

--- Deployment/Script_create_grant.sql
+++ Deployment/Script_create_grant.sql
@@ -2,13 +2,13 @@
     Autor:          Juan Esteban Nichoy Larrañaga
     Fecha:          25/01/2021
     Ejecutar en:    Script para Dockerfile
-    Descripción:    Crear permisos sobre las tablas para el usuario DBATESTS
+    Descripción:    Crear permisos sobre las tablas para el usuario DBADEV
 */
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON proyecto_grado_api_test.NODO_SENSOR TO 'DBATESTS'@'%';
-GRANT SELECT, INSERT, UPDATE, DELETE ON proyecto_grado_api_test.VARIABLES_NODO_SENSOR TO 'DBATESTS'@'%';
-GRANT SELECT, INSERT, UPDATE, DELETE ON proyecto_grado_api_test.CONFIGURACION_VARIABLES_NODO_SENSOR TO 'DBATESTS'@'%'; 
-GRANT SELECT, INSERT, UPDATE, DELETE ON proyecto_grado_api_test.REGLAS_NODO_SENSOR TO 'DBATESTS'@'%'; 
-GRANT SELECT, INSERT, UPDATE, DELETE ON proyecto_grado_api_test.DATOS_NODO_SENSOR TO 'DBATESTS'@'%';
-GRANT SELECT, INSERT, UPDATE, DELETE ON proyecto_grado_api_test.ERRORES_LOG_EJECUCION TO 'DBATESTS'@'%'; 
-GRANT SELECT, INSERT ON proyecto_grado_api_test.REPORTE_LOG_EJECUCION TO 'DBATESTS'@'%';
+GRANT SELECT, INSERT, UPDATE, DELETE ON proyecto_grado_api_test.NODO_SENSOR TO 'DBADEV'@'%';
+GRANT SELECT, INSERT, UPDATE, DELETE ON proyecto_grado_api_test.VARIABLES_NODO_SENSOR TO 'DBADEV'@'%';
+GRANT SELECT, INSERT, UPDATE, DELETE ON proyecto_grado_api_test.CONFIGURACION_VARIABLES_NODO_SENSOR TO 'DBADEV'@'%'; 
+GRANT SELECT, INSERT, UPDATE, DELETE ON proyecto_grado_api_test.REGLAS_NODO_SENSOR TO 'DBADEV'@'%'; 
+GRANT SELECT, INSERT, UPDATE, DELETE ON proyecto_grado_api_test.DATOS_NODO_SENSOR TO 'DBADEV'@'%';
+GRANT SELECT, INSERT, UPDATE, DELETE ON proyecto_grado_api_test.ERRORES_LOG_EJECUCION TO 'DBADEV'@'%'; 
+GRANT SELECT, INSERT ON proyecto_grado_api_test.REPORTE_LOG_EJECUCION TO 'DBADEV'@'%';
